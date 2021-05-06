bc56c6aeb5938f3822621bb64b0a3134423658dc
dubandro <dubovik@internet.ru>
2021-05-06 18:07:36 +0200

    JDBCAuthService - будущий сервис авторизации через БД

---
+++ ChatServer/src/main/java/ru/geekbrains/dubovik/appchat/server/authentication/JDBCAuthService.java
@@ -0,0 +1,21 @@
+package ru.geekbrains.dubovik.appchat.server.authentication;
+
+/**
+ * заглушка - доделаю в ближайшее время
+ */
+public class JDBCAuthService implements AuthService{
+    @Override
+    public void start() {
+        
+    }
+
+    @Override
+    public void stop() {
+
+    }
+
+    @Override
+    public String getUsernameByLoginPass(String login, String pass) {
+        return null;
+    }
+}
